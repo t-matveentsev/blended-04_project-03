d13cc272047d087e9065e47c037094a4ef278342
t-matveentsev <tmatveentsev@gmail.com>
2024-01-16 08:19:00 +0100

    btn-mobile-fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-const buttonMenuClose = document.querySelector(`.mobile-menu-btn`);
+const buttonMenuClose = document.querySelector(`.close-menu-btn`);
 const menuBlock = document.querySelector(`.mobile-menu`);
-const buttonMenuOpen = document.querySelector(`.menu-btn`);
-buttonMenuOpen.addEventListener("click", function(e){
-    menuBlock.classList.toggle("is-open");
-}
-)
-buttonMenuClose.addEventListener("click", function(e){    menuBlock.classList.toggle("is-open");
-}
-)
+const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
+const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
+buttonMenuOpen.addEventListener("click", function (e) {
+  menuBlock.classList.toggle("is-open");
+});
+buttonMenuOpenTab.addEventListener("click", function (e) {
+  menuBlock.classList.toggle("is-open");
+});
+buttonMenuClose.addEventListener("click", function (e) {
+  menuBlock.classList.toggle("is-open");
+});
