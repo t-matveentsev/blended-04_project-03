c84e6e2deaf68d44e99e8170fde6ab6ed00a7eff
t-matveentsev <tmatveentsev@gmail.com>
2024-03-14 19:10:17 +0100

    update_close_menu

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
+// const buttonMenuClose = document.querySelector(`.close-menu-btn`);
+// const menuBlock = document.querySelector(`.mobile-menu`);
+// const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
+// const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
+// buttonMenuOpen.addEventListener("click", function (e) {
+//   menuBlock.classList.toggle("is-open");
+// });
+// buttonMenuOpenTab.addEventListener("click", function (e) {
+//   menuBlock.classList.toggle("is-open");
+// });
+// buttonMenuClose.addEventListener("click", function (e) {
+//   menuBlock.classList.toggle("is-open");
+// });
+
+
 const buttonMenuClose = document.querySelector(`.close-menu-btn`);
 const menuBlock = document.querySelector(`.mobile-menu`);
 const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
 const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
-buttonMenuOpen.addEventListener("click", function (e) {
-  menuBlock.classList.toggle("is-open");
-});
-buttonMenuOpenTab.addEventListener("click", function (e) {
-  menuBlock.classList.toggle("is-open");
-});
-buttonMenuClose.addEventListener("click", function (e) {
-  menuBlock.classList.toggle("is-open");
-});
+
+const closeMenu = document.querySelector(window)
+
+const listenFoo = () => {
+  menuBlock.classList.toggle("is-open")
+};
+buttonMenuOpen.addEventListener("click", listenFoo);
+buttonMenuOpenTab.addEventListener("click", listenFoo);
+buttonMenuClose.addEventListener("click", listenFoo);
+
+closeMenu.addEventListener("click", listenFoo)
