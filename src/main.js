7502dadd98c42ed7aa1c272c896ea0e5bf358557
t-matveentsev <tmatveentsev@gmail.com>
2024-03-14 19:33:24 +0100

    update_close

--- src/main.js
+++ src/main.js
@@ -1,30 +1,14 @@
-// const buttonMenuClose = document.querySelector(`.close-menu-btn`);
-// const menuBlock = document.querySelector(`.mobile-menu`);
-// const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
-// const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
-// buttonMenuOpen.addEventListener("click", function (e) {
-//   menuBlock.classList.toggle("is-open");
-// });
-// buttonMenuOpenTab.addEventListener("click", function (e) {
-//   menuBlock.classList.toggle("is-open");
-// });
-// buttonMenuClose.addEventListener("click", function (e) {
-//   menuBlock.classList.toggle("is-open");
-// });
-
-
 const buttonMenuClose = document.querySelector(`.close-menu-btn`);
 const menuBlock = document.querySelector(`.mobile-menu`);
 const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
 const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
-
-const closeMenu = document.querySelector(`.mob-nav-link`)
+const closeMenu = document.querySelector(`.menu-list`)
 
 const listenFoo = () => {
   menuBlock.classList.toggle("is-open")
 };
+
 buttonMenuOpen.addEventListener("click", listenFoo);
 buttonMenuOpenTab.addEventListener("click", listenFoo);
 buttonMenuClose.addEventListener("click", listenFoo);
-
 closeMenu.addEventListener("click", listenFoo)
