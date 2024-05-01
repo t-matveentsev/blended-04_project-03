1d3565a57bae0b58cf5090b93bfe4493dc4375f3
t-matveentsev <tmatveentsev@gmail.com>
2024-05-01 23:48:07 +0200

    fix/close-menu

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-const buttonMenuClose = document.querySelector(`.close-menu-btn`);
-const menuBlock = document.querySelector(`.mobile-menu`);
 const buttonMenuOpen = document.querySelector(`.open-menu-btn`);
 const buttonMenuOpenTab = document.querySelector(`.open-menu-btn-tab`);
-const closeMenu = document.querySelector(`.menu-list`)
+const menuBlock = document.querySelector(`.mobile-menu`);
+const closeMenu = document.querySelectorAll(`.mob-nav-link`);
+const buttonMenuClose = document.querySelector(`.close-menu-btn`);
 
 const listenFoo = () => {
-  menuBlock.classList.toggle("is-open")
+  menuBlock.classList.toggle('is-open');
 };
 
-buttonMenuOpen.addEventListener("click", listenFoo);
-buttonMenuOpenTab.addEventListener("click", listenFoo);
-buttonMenuClose.addEventListener("click", listenFoo);
-closeMenu.addEventListener("click", listenFoo)
+closeMenu.forEach(element => {
+  element.addEventListener('click', listenFoo);
+});
+
+function closeModal(event) {
+  const target = event.target;
+  if (
+    target.classList.contains('mobile-menu') ||
+    target.closeMenu ||
+    target.buttonMenuClose
+  ) {
+    menuBlock.classList.toggle('is-open');
+  }
+}
+
+menuBlock.addEventListener('click', closeModal);
+buttonMenuOpen.addEventListener('click', listenFoo);
+buttonMenuOpenTab.addEventListener('click', listenFoo);
+buttonMenuClose.addEventListener('click', listenFoo);
